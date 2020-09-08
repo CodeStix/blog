6f38ed6563586297c5b666007a30a75acb7dccd5
Stijn Rogiest <reddusted@gmail.com>
2020-09-08 15:18:19 +0200

    Basic project list

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,27 @@
 import React, { useState } from "react";
 import { GetStaticProps } from "next";
-import ReactMarkdown from "react-markdown";
-import hightlight from "remark-highlight.js";
+import NavBar from "../components/NavBar";
+import CenterContainer from "../components/CenterContainer";
+import BigTitle from "../components/BigTitle";
+import Title from "../components/Title";
+import Card from "../components/Card";
 
 export default function Index() {
-    const [input, setInput] = useState("# Test");
-
     return (
-        <div>
-            <h2>Enter text:</h2>
-            <textarea onChange={(e) => setInput(e.target.value)} value={input}></textarea>
-            <ReactMarkdown source={input} plugins={[hightlight]} renderers={{ code: hightlight }} />
-        </div>
+        <>
+            <NavBar />
+            <CenterContainer>
+                <Title title="Projects" />
+                <Card color="#00A3FF">
+                    <Title title="Beat-360fyer" color="#00A3FF" />
+                    <p>Use this tool to convert a standard Beat Saber level into a 360 degree one!</p>
+                </Card>
+                <Card color="#FFA800">
+                    <Title title="Reddit Discord bot" color="#FFA800" />
+                    <p>An amazing Discord bot that connects with reddit. Has video downloading support, 50/50 spoilers ...</p>
+                </Card>
+            </CenterContainer>
+        </>
     );
 }
 
