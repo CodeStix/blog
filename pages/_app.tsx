0059f085ef216de479bc5dcec0eb50c0f0b236f7
Stijn Rogiest <reddusted@gmail.com>
2020-09-10 16:59:24 +0200

    Fixed fontawesome font sizes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,8 @@ import "../css/styles.scss";
 import Head from "next/head";
 import "highlight.js/styles/monokai.css";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { config, dom } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false; // https://github.com/FortAwesome/react-fontawesome/issues/284
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
@@ -13,6 +15,7 @@ export default function MyApp({ Component, pageProps }) {
                     href="https://fonts.googleapis.com/css2?family=Poppins&family=Rubik&display=swap"
                     rel="stylesheet"
                 />
+                <style>{dom.css()}</style>
                 <title>Stijn Rogiest / codestix</title>
                 <meta
                     name="description"
