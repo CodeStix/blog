7fa42d67983976e1d8978c7d1392a696a5d5d93b
Stijn Rogiest <reddusted@gmail.com>
2020-09-13 17:11:01 +0200

    Index recent & lexical analysis article

---
+++ articles/javascript-lexical-analysis.md
@@ -0,0 +1,22 @@
+---
+name: Building a lexical analyser in javascript (compiler design)
+description: How does a compiler understand your code? How does it know what machine-code it should generate? This tutorial explains how you can create your own lexical analyser in javascript.
+themeColor: "#FFA3FF"
+thumbnails:
+    - /image/lexical-analysis-javascript/tokens.png
+type: tutorial
+readMinutes: 6
+githubRepo: CodeStix/shithead-the-game
+---
+
+## What is lexical analysis?
+
+![tokens](/image/lexical-analysis-javascript/tokens.png)
+
+Lexical analysis is the method to describe a sequence of characters (or text) as multiple tokens, where a token is a sequence of predefined characters.
+
+This type of analysis is used in compilers (the program that converts human-readable code into machine-readable code), because they don't understand individual characters but they may understand sequences of predefined characters. We tell the lexical analyser what tokens should be expected, we define these tokens, and then, we give the analyser some text and it lets us know if there is a syntax error and what tokens were used.
+
+## Lets develop our own!
+
+Lets develop our own lexical analyser in javascript!
--- pages/index.tsx
+++ pages/index.tsx
@@ -12,19 +12,28 @@ import { Article } from "../shared/Article";
 type IndexProps = {
     recentProjects: Article[];
     recentPosts: Article[];
+    recent: Article[];
 };
 
-export default function Index({ recentProjects, recentPosts }: IndexProps) {
+export default function Index({
+    recentProjects,
+    recentPosts,
+    recent,
+}: IndexProps) {
     return (
         <>
             <NavBar />
             <CenterContainer>
-                <Title>Recent posts</Title>
+                {recent.map((post) => (
+                    <ArticleCard key={post.name} article={post} />
+                ))}
+
+                {recentPosts.length > 0 && <Title>Recent posts</Title>}
                 {recentPosts.map((post) => (
                     <ArticleCard key={post.name} article={post} />
                 ))}
 
-                <Title>Recent projects</Title>
+                {recentProjects.length > 0 && <Title>Recent projects</Title>}
                 {recentProjects.map((project) => (
                     <ArticleCard key={project.name} article={project} />
                 ))}
@@ -38,6 +47,7 @@ export const getStaticProps: GetStaticProps = async function () {
     var articles = await getArticles();
     articles = articles.sort((a, b) => b.modified - a.modified);
     var props: IndexProps = {
+        recent: articles.splice(0, 2),
         recentProjects: articles
             .filter((e) => e.type === "project")
             .splice(0, 2),
