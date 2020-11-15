7641b073d4094413727f24f4f70a7cda10b51deb
Stijn Rogiest <reddusted@gmail.com>
2020-11-15 13:41:33 +0100

    Removed unnecessary Title implementation + enable margin on first child again

--- components/Title.tsx
+++ components/Title.tsx
@@ -1,21 +1,9 @@
-import React from "react";
 import styled from "styled-components";
 
-const TitleHeader = styled.h2`
+const Title = styled.h2`
+    display: block;
     margin: 1em 0em 0.5em 0em;
-    color: ${(props) => props.color ?? "white"};
     font-size: 1.8em;
-
-    &:first-child {
-        margin-top: 0;
-    }
 `;
 
-type TitleProps = {
-    children: React.ReactNode;
-    color?: string;
-};
-
-export default function Title({ children, color }: TitleProps) {
-    return <TitleHeader color={color}>{children}</TitleHeader>;
-}
+export default Title;
